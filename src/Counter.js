6642ad748c6673e14392ab5e9921ec1fa857c55b
Eduardo Silva <educwb@gmail.com>
2021-08-23 11:24:37 -0300

    adicionando primeiro exemplo do useEffect

--- src/Counter.js
+++ src/Counter.js
@@ -1,4 +1,4 @@
-import React, { useState} from 'react'
+import React, { useState, useEffect } from 'react'
 
 import './Counter.css'
 
@@ -7,6 +7,15 @@ const Counter = () => {
   // na 1ª posição (contador), me retorna o estado atual
   // na 2ª posição (setContador), me retorna uma função para alterar esse estado
   const [contador, setContador] = useState(0)
+
+  // recebe dois parâmetros
+  // 1º é uma função de callback que será executada antes do restante
+  // 2º é um array de dependências
+  // toda vez que a variável do array for alterada,
+  // o callback será executado novamente
+  useEffect(() => {
+    console.log('renderizou')
+  }, [(contador)])
 
   const handleOnClick = operador => {
     let novoValor = operador === '+' ? contador + 1 : contador -1
