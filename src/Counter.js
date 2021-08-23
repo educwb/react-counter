51a46b4ab072818141e291fd00578c4b223efbd1
Eduardo Silva <educwb@gmail.com>
2021-08-23 10:59:50 -0300

    adicionando alteracao do useState no contador

--- src/Counter.js
+++ src/Counter.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useState} from 'react'
 
 import './Counter.css'
 
 const Counter = () => {
+  // useState é um método que retorna um array
+  // na 1ª posição (contador), me retorna o estado atual
+  // na 2ª posição (setContador), me retorna uma função para alterar esse estado
+  const [contador, setContador] = useState(0)
 
   const handleOnClick = operador => {
-    alert(operador)
+    let novoValor = operador === '+' ? contador + 1 : contador -1
+    setContador(novoValor)
   }
 
   return (
     <div className="counter">
-      <span>5</span>
+      <span>{contador}</span>
       <button onClick={() => handleOnClick('-')}>-</button>
       <button onClick={() => handleOnClick('+')}>+</button>
     </div>
