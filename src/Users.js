e496812dde566917f48d19ccbde46493bd0b05e1
Eduardo Silva <educwb@gmail.com>
2021-08-23 20:39:35 -0300

    adicionando página Users

--- src/Users.js
+++ src/Users.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-const Users = () => {
+const Albuns = () => {
+  const [loading, setLoading] = useState(false)
+  const [users, setUsers] = useState([]) // no vídeo foi necessário useState([]) para não dá erro
+
+  useEffect(() => {
+    setLoading(true)
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => {
+        setUsers(data)
+        setLoading(false)
+      })
+  }, [])
   return (
-    <h1>Users</h1>
+    <div>
+      <h1>Usuários</h1>
+      {
+        loading === true ? 'Carregando...' : ''
+      }
+      {
+        users.map(user => {
+          return (
+            <div style={{margin:'10px'}}>
+              <b>Nome:</b> {user.name} <br />
+              <b>Email:</b> {user.email} <br />
+              <b>Telefone:</b> {user.phone} <br />
+              <b>Site:</b> {user.website} <br />
+            </div>
+          )
+        })
+      }
+    </div>
   )
 }
 
-export default Users
+export default Albuns
