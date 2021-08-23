52e9559fc70164342306cd7de0d2d5e3838db835
Eduardo Silva <educwb@gmail.com>
2021-08-23 17:36:00 -0300

    acessando API - concatenando json no then

--- src/Albums.js
+++ src/Albums.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Albuns = () => {
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/albums')
+      // .then(response => {
+      //   console.log(response)
+      // })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+      })
+  }, [])
   return (
     <h1>Álbuns</h1>
   )
