1ce0492f54fc005d5b8be6a2760f8ae7f717e0b8
Eduardo Silva <educwb@gmail.com>
2021-08-23 11:59:33 -0300

    adicionando exemplo de alterar valor da variavel pelo botao

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 import Counter from './Counter'
+import Albuns from './Albuns'
 
 function App() {
+  const [page, setPage] = useState()
+
+  const handlePageChange = page => {
+    setPage(page)
+  }
   return (
     <>
+      <h2>{page}</h2>
+      <button onClick={() => handlePageChange('albuns')}>Álbuns</button>
+      <button onClick={() => handlePageChange('contador')}>Contador</button>
       <Counter />
+      <Albuns />
     </>
   )
 }
